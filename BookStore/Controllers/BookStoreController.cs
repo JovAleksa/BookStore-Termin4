bd9d1f845d9197bd86b5ddb30fe362cc1beb9b32
Sale <Sale@LAPTOP-LENOVO>
2023-01-31 11:18:08 +0100

    Add project files.

---
+++ BookStore/Controllers/BookStoreController.cs
@@ -0,0 +1,91 @@
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace BookStore.Controllers
+{
+    public class BookStoreController : Controller
+    {
+        public static List<BookModel> books = new List<BookModel>();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken()]
+        public IActionResult Add(BookModel book)
+        {
+            if (!ModelState.IsValid)
+            {
+                if (book.Name == null)
+                {
+                    ViewBag.FirstNameError = "Please write name of book";
+                }
+                else if(book.Price==0)
+                {
+                    ViewBag.SecondNameError = "Please write book price ";
+                }
+                return View("index");
+            }
+            if (books.Count == 0)
+            {
+                books.Add(book);
+            }
+            else
+            {
+                foreach (BookModel bookModel in books)
+                {
+                    if (bookModel.Name.Contains(book.Name))
+                    {
+                        ViewBag.FirstNameError = "The book already exists.";
+                        return View("index");
+
+                    }                 
+
+                }
+                books.Add(book);
+            }
+
+            return RedirectToAction("ListAll");
+        }
+        public IActionResult Delete(int Id, string fname)
+        {
+            foreach (BookModel bm in books)
+            {
+                if (bm.id == Id)
+                {
+                    bm.Deleted = true;
+                    break;
+                }
+            }
+            return RedirectToAction(fname);
+        }
+      
+        public IActionResult ListAll()
+        {
+
+            ViewBag.book = books;
+            return View();
+        }
+
+        public IActionResult DeleteList()
+        {
+            ViewBag.book = books;
+            return View();
+
+        }
+
+        public IActionResult Sortiraj(int smer, string fname)
+        {
+            List<BookModel> sortirani = new List<BookModel>();
+            sortirani = smer == 1 ? books.OrderBy(x => x.Name).ToList() : smer == 2 ? books.OrderByDescending(x => x.Name).ToList() : smer == 3 ? books.OrderBy(x => x.Price).ToList() : books.OrderByDescending(x => x.Price).ToList();
+            ViewBag.book = sortirani;
+            return View(fname);
+        }
+       
+
+
+    }
+}
---
+++ BookStore/Models/BookModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookStore.Models
+{
+    [NotMapped]
+    public class BookModel
+    {
+
+        [Required]
+        public String Name { get; set; }
+        public static int brojacID = 0;
+        public  int id { get; set; }
+
+        public Genre genre { get; set; }
+        public enum Genre { Science =1, Comedy=2, Horror=3 }
+        [Required]
+        public Double Price { get; set; }
+        public bool Deleted { get; set; }
+
+     
+        public BookModel()
+        {
+            brojacID++;
+            id = brojacID;
+            
+
+        }
+
+    }
+}
